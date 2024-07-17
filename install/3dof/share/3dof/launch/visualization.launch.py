f700f7de61f6180f0a6b72da644e9ccd9c50e01b
Nelsordo <nelson.ureta1901@alumnos.ubiobio.cl>
2024-07-17 18:40:08 -0400

    comentado

--- install/3dof/share/3dof/launch/visualization.launch.py
+++ install/3dof/share/3dof/launch/visualization.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     urdf_path = '/home/ubb/Downloads/ros2_ws_final/ros2_ws/src/3dof/urdf/robot.urdf'
     with open(urdf_path,'r') as i:
         robot_desc = i.read()
-
+#Este es el nodo con el diseño del robot
     pub = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -17,19 +17,20 @@ def generate_launch_description():
             'robot_description': robot_desc
         }]
     )
-
+#Este nodo abre los controles de las articulaciones del brazo
     joint = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
         name="joint_state_publisher_gui",
         output='screen'
     )
-
+#con esta linea se ejecuta rviz para simular el brazo
     rviz = ExecuteProcess(
         cmd=['rviz2', '-d', '/home/ubb/Downloads/ros2_ws_final/ros2_ws/src/3dof/config/config.rviz'],
         output='screen'
     )
 
+#Se ejecutan los procesos para la simulación en rviz
     return LaunchDescription([
         pub,
         joint,
--- src/3dof/launch/gazebo.launch.py
+++ src/3dof/launch/gazebo.launch.py
@@ -4,9 +4,12 @@ from launch.actions import ExecuteProcess
 from launch.substitutions import Command
 from launch_ros.actions import Node
 
+#En este código se inician los nodos necesarios para la simulación en gazebo 
+
 def generate_launch_description():
     urdf_path = '/home/ubb/Downloads/ros2_ws_final/ros2_ws/src/3dof/urdf/robot.xacro'
 
+#Este es el nodo con el diseño del robot
     pub = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -14,25 +17,27 @@ def generate_launch_description():
             'robot_description': Command(['xacro ', urdf_path])
         }]
     )
-
+#Este nodo recibe el diseño del robot para simularlo en gazebo
     spawn_entity = Node(
         package="gazebo_ros",
         executable="spawn_entity.py",
         arguments=['-entity', 'robot', '-topic', 'robot_description'],
         output='screen'
     )
-
+#en estas dos lineas se inicia gazebo en un mundo personalizado
     world = '/home/ubb/Downloads/ros2_ws_final/ros2_ws/src/3dof/world/obstacule.world'
     gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', world,'-s', 'libgazebo_ros_factory.so'], output='screen')
 
+#Este nodo envía una posición constante al topico /joint_trajectory para mover al brazo a una
+#posicion designada
     operacion = Node(
             package='3dof',
             executable='operacion',
             name='operacion',
             output='screen'
         )
-#)
 
+#Este nodo procesa los datos del robot para simular la posición de las articulaciones
     controller_manager = Node(
         package='controller_manager',
         executable='spawner',
@@ -40,7 +45,7 @@ def generate_launch_description():
             'joint_trajectory_controller'
         ],
     )
-    
+#Este nodo envia la posición calculada por controller_manager a gazebo para visualizarla en la simulación
     joint_broad_controller = Node(
         package='controller_manager',
         executable='spawner',
@@ -48,11 +53,12 @@ def generate_launch_description():
             'joint_state_broadcaster'
         ],
     )
+#Se ejecutan los procesos para la simulación en gazebo
     return LaunchDescription([
         pub,
         gazebo,
         spawn_entity,
         controller_manager,
         joint_broad_controller,
-        #operacion
+        #operacion #Esta linea es para enviar una posición de forma permanente, en caso de quere usar otra posición, se comenta esta linea.
     ])
